5183204314431d2c0b8d9f06ca06bccd4afa22cf
981545521@qq.com <Fy52007825>
2020-08-25 16:22:00 +0800

    修改配置

--- src/main/java/cn/acyou/aquarius/order/controller/OrderController.java
+++ src/main/java/cn/acyou/aquarius/order/controller/OrderController.java
@@ -9,14 +9,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author youfang
  * @version [1.0.0, 2020/8/20]
  **/
 @RestController
-@RequestMapping("order")
+@RequestMapping("test")
 public class OrderController {
     @Autowired
     private OrderMapper orderMapper;
@@ -25,5 +27,13 @@ public class OrderController {
     public Result<?> list() {
         List<Order> orders = orderMapper.selectAll();
         return Result.success(orders);
+    }
+    @GetMapping(value = "/detail")
+    public Result<?> detail() {
+        Map<String, String> detailInfo = new HashMap<>();
+        for (int i = 0; i < 50; i++) {
+            detailInfo.put("订单详情" + i, String.valueOf(i));
+        }
+        return Result.success(detailInfo);
     }
 }
