e6800b16e202611a2715fb1dd3aa372b23a61caa
981545521@qq.com <Fy52007825>
2020-08-25 17:48:59 +0800

    update

--- src/main/java/cn/acyou/aquarius/order/controller/OrderController.java
+++ src/main/java/cn/acyou/aquarius/order/controller/OrderController.java
@@ -4,6 +4,7 @@ import cn.acyou.aquarius.order.common.Result;
 import cn.acyou.aquarius.order.entity.Order;
 import cn.acyou.aquarius.order.mapper.OrderMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,9 @@ public class OrderController {
     @Autowired
     private OrderMapper orderMapper;
 
+    @Value("${local.ip}")
+    private String localIp;
+
     @GetMapping(value = "/list")
     public Result<?> list() {
         List<Order> orders = orderMapper.selectAll();
@@ -31,7 +35,8 @@ public class OrderController {
     @GetMapping(value = "/detail")
     public Result<?> detail() {
         Map<String, String> detailInfo = new HashMap<>();
-        for (int i = 0; i < 50; i++) {
+        detailInfo.put("我是服务", localIp);
+        for (int i = 0; i < 10; i++) {
             detailInfo.put("订单详情" + i, String.valueOf(i));
         }
         return Result.success(detailInfo);
